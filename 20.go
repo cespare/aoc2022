72a45d131706f482f6ef25a6bf82a2c7708ee162
Caleb Spare <cespare@gmail.com>
2022-12-20 02:25:05 -0600

    Simplify 20 by embracing the circle

--- 20.go
+++ 20.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"golang.org/x/exp/slices"
-)
-
 func init() {
 	addSolutions(20, problem20)
 }
@@ -29,8 +25,6 @@ func problem20(ctx *problemContext) {
 
 type encFile struct {
 	nodes []*encFileNode // in original order
-	head  *encFileNode
-	tail  *encFileNode
 }
 
 type encFileNode struct {
@@ -41,38 +35,38 @@ type encFileNode struct {
 
 func newEncFile(nums []int) *encFile {
 	f := &encFile{nodes: make([]*encFileNode, len(nums))}
+	var prev *encFileNode
 	for i, n := range nums {
 		node := &encFileNode{n: n}
 		f.nodes[i] = node
-		if f.head == nil {
-			f.head = node
+		node.prev = prev
+		if prev != nil {
+			prev.next = node
 		}
-		if f.tail != nil {
-			f.tail.next = node
-			node.prev = f.tail
-		}
-		f.tail = node
+		prev = node
 	}
+	f.nodes[0].prev = f.nodes[len(f.nodes)-1]
+	f.nodes[len(f.nodes)-1].next = f.nodes[0]
 	return f
 }
 
-func (f *encFile) slice() []int {
-	nums := make([]int, 0, len(f.nodes))
-	for node := f.head; node != nil; node = node.next {
-		nums = append(nums, node.n)
-	}
-	return nums
-}
-
 func (f *encFile) answer() int {
-	s := f.slice()
-	iz := slices.Index(s, 0)
-	if iz < 0 {
-		panic("zero gone")
+	var zero *encFileNode
+	for _, node := range f.nodes {
+		if node.n == 0 {
+			zero = node
+			break
+		}
+	}
+	var i int
+	ordered := make([]int, len(f.nodes))
+	for node := zero; i == 0 || node != zero; node = node.next {
+		ordered[i] = node.n
+		i++
 	}
 	get := func(afterZero int) int {
-		i := (iz + afterZero) % len(f.nodes)
-		return s[i]
+		i := afterZero % len(ordered)
+		return ordered[i]
 	}
 	return get(1000) + get(2000) + get(3000)
 }
@@ -94,60 +88,24 @@ func (f *encFile) mixAt(node *encFileNode) {
 	}
 }
 
-// All methods below assume a reasonable list length (>3, at least) for
-// simplicity.
-
 func (f *encFile) moveLeft(node *encFileNode) {
-	f.delete(node)
-	switch {
-	case node.prev == nil:
-		f.insertAfter(node, f.tail.prev)
-	case node.prev.prev == nil:
-		f.insertAfter(node, f.tail)
-	default:
-		f.insertAfter(node, node.prev.prev)
-	}
+	node.remove()
+	node.insertAfter(node.prev.prev)
 }
 
 func (f *encFile) moveRight(node *encFileNode) {
-	f.delete(node)
-	switch {
-	case node.next == nil:
-		f.insertAfter(node, f.head)
-	case node.next.next == nil:
-		f.insertHead(node)
-	default:
-		f.insertAfter(node, node.next)
-	}
+	node.remove()
+	node.insertAfter(node.next)
 }
 
-func (f *encFile) delete(node *encFileNode) {
-	if node.prev == nil {
-		f.head = node.next
-	} else {
-		node.prev.next = node.next
-	}
-	if node.next == nil {
-		f.tail = node.prev
-	} else {
-		node.next.prev = node.prev
-	}
+func (n *encFileNode) remove() {
+	n.prev.next = n.next
+	n.next.prev = n.prev
 }
 
-func (f *encFile) insertHead(node *encFileNode) {
-	node.prev = nil
-	node.next = f.head
-	f.head.prev = node
-	f.head = node
-}
-
-func (f *encFile) insertAfter(node, left *encFileNode) {
-	if left.next == nil {
-		f.tail = node
-	} else {
-		left.next.prev = node
-	}
-	node.next = left.next
-	left.next = node
-	node.prev = left
+func (n *encFileNode) insertAfter(left *encFileNode) {
+	left.next.prev = n
+	n.next = left.next
+	left.next = n
+	n.prev = left
 }
